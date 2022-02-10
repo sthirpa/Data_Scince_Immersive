139ace2f2e8f8f627bad2ebf76033fd6a0f316a1
sileshith <hirpast@gmail.com>
2022-02-10 15:07:58 -0800

    adding class notes

---
+++ Class-Notes/breakfast-hour/06-week/largest_non_adj_sum.py
@@ -0,0 +1,40 @@
+'''
+Problem from GitHub user @vineetjohn
+This problem was asked by Airbnb.
+
+Given a list of integers, write a function that returns the largest sum of two non-adjacent numbers. Numbers can be 0 or negative.
+
+For example, [2, 4, 6, 8] should return 12, since we pick 4 and 8. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+>>> largest_non_adj_sum([2, 4, 6, 8])
+12
+
+>>> largest_non_adj_sum([5, 1, 1, 5])
+10
+
+>>> largest_non_adj_sum([2, 4, 6, 3, 8, 9])
+15
+
+>>> largest_non_adj_sum([1, 2])
+2
+
+>>> largest_non_adj_sum([1])
+1
+
+>>> largest_non_adj_sum([])
+0
+
+'''
+
+def largest_non_adj_sum(array):
+
+    # Replace the line below with all your code. Remember to return the requested data.
+    pass
+
+
+largest_non_adj_sum([2, 4, 6, 8])
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
