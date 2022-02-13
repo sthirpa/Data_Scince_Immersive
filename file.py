7faaeb929e3e2b8b351d4bccbaeb5f07828c1968
SILESHI HIRPA <54763081+sthirpa@users.noreply.github.com>
2022-02-13 13:31:40 -0800

    streamlit for predicting from local machine

---
+++ file.py
@@ -0,0 +1,50 @@
+# import the necessary Package
+import os
+import cv2
+import numpy as np
+from PIL import Image , ImageOps
+import streamlit as st
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+
+
+# Create a function to load my saved model
+@st.cache(allow_output_mutation=True)
+def load_my_model():
+    model = tf.keras.models.load_model("models/bestModel.h5")
+    return model
+
+model = load_my_model()
+
+# Create a title of web App and Introductory note.
+st.title('Welcome to my Image Classification Project!')
+st.text('This app classifies images whether they belong to any of the 10 classes on which my ')
+st.text('model was trained. The dataset I used for my capstone project was ')
+st.text('the CIFAR-10 image dataset and the class names are:')
+st.text('class_names = ["airplane", "automobile" , "bird" , "cat" , "deer" , "dog",')
+st.text('"frog" ,horse" , "ship" , "truck"].')
+st.text("My model is expected to predict with 86% accuracy, but let's see :)")
+st.subheader("Please Upload images related to this class_names...")
+
+# create a file uploader and take a image as an jpg or png
+file = st.file_uploader("Upload the image" , type=["jpg" , "png"])
+
+# Create a function to take an image and predict the class
+def import_and_predict(image_data , model):
+    size = (32 ,32)
+    image = ImageOps.fit(image_data , size , Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    return prediction
+
+if st.button("Predict"):
+    image = Image.open(file)
+    st.image(image , use_column_width=True)
+    predictions = import_and_predict(image , model)
+
+    class_names = ["airplane", "automobile" , "bird" , "cat" , "deer" , "dog" , "frog" , "horse" , "ship" , "truck"]
+
+    string = "Image mostly looks like: " + class_names[np.argmax(predictions)]
+    st.success(string)
+# from: https://github.com/Sarthak-1408
